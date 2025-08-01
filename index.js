64d6280304d7a1a54d53866be6a32bee50910cbf
VictoriaJoy237 <vickyjoymbwana@gmail.com>
2025-08-01 16:07:53 +0200

    Styling the main section

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 // TODO: Add event listener for theme change button
+$("button").on("click",function(){
+    $(".toggle-button").addClass("new-button");
+});
 
 // TODO: Add jQuery code for highlighting list items
-
+$("li").on("click"),function(){
+    $("li").addClass(".hobby2 li")
+}
 // TODO: Add code for reset button
 
 // TODO: Add code for adding new list items from form
